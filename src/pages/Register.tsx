92cd12e6626e1a0424cd499f96823c92801f5932
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2025-04-20 20:50:50 +0100

    Fixed build error

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -5,7 +5,7 @@ import supabase from "../utils";
 const Register = () => {
   const [formSubmitted, setFormSubmitted] = useState(false);
   const [delgeateOption, setDelegateOption] = useState<string>("");
-  const [formData, setFormData] = useState<any>(null);
+  const [ setFormData] = useState<any>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   
